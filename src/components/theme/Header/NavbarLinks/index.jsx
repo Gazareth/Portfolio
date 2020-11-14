df807b72226a9870a2a916750cf9e20507994943
prsnk <persnicketygareth@gmail.com>
2020-11-14 12:29:39 +0000

    feat(navbar): Add blobs page to navbar.

--- src/components/theme/Header/NavbarLinks/index.jsx
+++ src/components/theme/Header/NavbarLinks/index.jsx
@@ -9,9 +9,10 @@ const NavbarLinks = ({ desktop }) => {
 
   return (
     <Wrapper desktop={desktop} theme={theme}>
-      <AnchorLink href="#about">About</AnchorLink>
       <AnchorLink href="#projects">Projects</AnchorLink>
+      <AnchorLink href="#about">About</AnchorLink>
       <AnchorLink href="#contact">Contact</AnchorLink>
+      <AnchorLink href="#blobs">Blobs</AnchorLink>
       <ToggleTheme />
     </Wrapper>
   )
